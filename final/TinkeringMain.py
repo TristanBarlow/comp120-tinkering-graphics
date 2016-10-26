651d8fa9d3e7f928a24eb2b96b9b5a9df3bd3532
TristanBarlow <tristanbarlowg@gmail.com>
2016-10-26 11:43:38 +0100

    Removed features from main game loop Added features as functions, so main game loop is neater improved a few variable names

--- final/TinkeringMain.py
+++ final/TinkeringMain.py
@@ -15,7 +15,7 @@ WIDTH = 900
 a=1
 b=452
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-picture = pygame.image.load("pic5.png")
+picture = pygame.image.load("pic4.png")
 picture_transformed = pygame.transform.scale(picture, (WIDTH,HEIGHT))
 window.blit(picture_transformed,(0,0))
 
@@ -36,6 +36,99 @@ def distance(r_1, r_2, g_1, g_2, b_1, b_2):
     return likeness
 
 
+def circle():
+    for y in range(0, HEIGHT / 2):
+        b = (HEIGHT+4)/2
+        b -= 1
+        x = WIDTH / 2
+        red = window.get_at((x, y)).r
+        green = window.get_at((x, y)).g
+        blue = window.get_at((x, y)).b
+        pygame.draw.circle(window, (red, green, blue), (WIDTH / 2, HEIGHT / 2), b, 1)
+
+
+def outline():
+    for x in xrange(1, WIDTH - 1):
+        for y in xrange(1, HEIGHT - 1):
+            px_array = pygame.PixelArray(window)
+            red = window.get_at((x, y)).r
+            green = window.get_at((x, y)).g
+            blue = window.get_at((x, y)).b
+            red_1 = window.get_at((x + 1, y)).r
+            green_1 = window.get_at((x + 1, y)).g
+            blue_1 = window.get_at((x + 1, y)).b
+            likeness = distance(red, red_1, green, green_1, blue, blue_1)
+            if likeness > 20:
+                px_array[x, y] = (0, 0, 0)
+            else:
+                px_array[x, y] = (255, 255, 255)
+
+
+def water_fall():
+    for y in xrange(1, HEIGHT - 1):
+        for x in xrange(1, WIDTH - 1):
+            red = window.get_at((x, y)).r
+            green = window.get_at((x, y)).g
+            blue = window.get_at((x, y)).b
+            red_1 = window.get_at((x, y + 1)).r
+            green_1 = window.get_at((x, y + 1)).g
+            blue_1 = window.get_at((x, y + 1)).b
+            likeness = distance(red, red_1, green, green_1, blue, blue_1)
+            pygame.draw.rect(window, (red_1, green_1, blue_1), (x + 1, y, 1, likeness / 10), )
+
+
+def horizontal_lines():
+    for y in xrange(1, HEIGHT - 1):
+        for x in xrange(1, WIDTH - 1):
+            red = window.get_at((x, y)).r
+            green = window.get_at((x, y)).g
+            blue = window.get_at((x, y)).b
+            red_1 = window.get_at((x + 1, y)).r
+            green_1 = window.get_at((x + 1, y)).g
+            blue_1 = window.get_at((x + 1, y)).b
+            likeness = distance(red, red_1, green, green_1, blue, blue_1)
+            pygame.draw.rect(window, (red_1, green_1, blue_1), (x + 1, y, likeness / 3, 1),1)
+
+
+def night_vision():
+    for y in xrange(1, HEIGHT - 1):
+        for x in xrange(1, WIDTH - 1):
+            red = window.get_at((x, y)).r
+            green = window.get_at((x, y)).g
+            blue = window.get_at((x, y)).b
+            red_final = red * 0.3
+            green_final = reduce_colours(green)
+            blue_final = green * 0.3
+            px_array[x, y] = (red_final, green_final, blue_final)
+
+
+def color_invert():
+    for y in xrange(1, HEIGHT - 1):
+        for x in xrange(1, WIDTH - 1):
+            red = window.get_at((x, y)).r
+            green = window.get_at((x, y)).g
+            blue = window.get_at((x, y)).b
+            red_final = green
+            blue_final = red
+            green_final = blue
+            px_array[x, y] = (red_final, green_final, blue_final)
+
+
+def blur_picture(a):
+    for y in xrange(1, HEIGHT - 1):
+        for x in xrange(1, WIDTH - 1):
+            red = window.get_at((x, y)).r
+            green = window.get_at((x, y)).g
+            blue = window.get_at((x, y)).b
+            red_1 = window.get_at((x + a, y + a)).r
+            green_1 = window.get_at((x + a, y + a)).g
+            blue_1 = window.get_at((x + a, y + a)).b
+            red_final = (red + red_1) / 2
+            green_final = (green + green_1) / 2
+            blue_final = (blue + blue_1) / 2
+            px_array[x:x + 1, y:y + 1] = (red_final, green_final, blue_final)
+
+
 while True:
     keys = pygame.key.get_pressed()
     px_array = pygame.PixelArray(window)
@@ -49,82 +142,34 @@ while True:
 # Blurs When 'w' is pressed
     if keys[pygame.K_w]:
         a = -a
-        for y in xrange(1,HEIGHT-1):
-            for x in xrange(1,WIDTH-1):
-                red = window.get_at((x, y)).r
-                green = window.get_at((x, y)).g
-                blue = window.get_at((x, y)).b
-                red_1 = window.get_at((x+a, y+a)).r
-                green_1 = window.get_at((x+a, y+a)).g
-                blue_1 = window.get_at((x+a, y+a)).b
-                red_final = (red+red_1)/2
-                green_final = (green+green_1)/2
-                blue_final = (blue+blue_1)/2
-                px_array[x:x+1,y:y+1] = (red_final, green_final, blue_final)
-                
+        blur_picture(a)
+
 # Inverts colours when 'q' is pressed
     if keys[pygame.K_q]:
-        for y in xrange(1,HEIGHT-1):
-            for x in xrange(1,WIDTH-1):
-                red = window.get_at((x, y)).r
-                green = window.get_at((x, y)).g
-                blue = window.get_at((x, y)).b
-                red_final = green
-                blue_final = red
-                green_final = blue
-                px_array[x,y] = (red_final, green_final, blue_final)
-                
+        color_invert()
+
 # Night Vision when e is pressed
     if keys[pygame.K_e]:
-        for y in xrange(1,HEIGHT-1):
-            for x in xrange(1,WIDTH-1):
-                red = window.get_at((x, y)).r
-                green = window.get_at((x, y)).g
-                blue = window.get_at((x, y)).b
-                red_final = red*0.3
-                green_final = reduce_colours(green)
-                blue_final = green*0.3
-                px_array[x,y] = (red_final, green_final, blue_final)
-                
+        night_vision()
+
 # Press r for vertical lines
     if keys[pygame.K_r]:
-        for y in xrange(1,HEIGHT-1):
-            for x in xrange(1,WIDTH-1):
-                red = window.get_at((x, y)).r
-                green = window.get_at((x, y)).g
-                blue = window.get_at((x, y)).b
-                red_1 = window.get_at((x, y+1)).r
-                green_1 = window.get_at((x, y+1)).g
-                blue_1 = window.get_at((x, y+1)).b
-                likeness = distance(red, red_1, green, green_1, blue, blue_1)
-                pygame.draw.rect(window, (red_1, green_1, blue_1), (x + 1 , y , 1 , likeness/10),)
-                
+        water_fall()
+
 # Press t for horizontal lines
     if keys[pygame.K_t]:
-        for y in xrange(1, HEIGHT-1):
-            for x in xrange(1, WIDTH-1):
-                red = window.get_at((x, y)).r
-                green = window.get_at((x, y)).g
-                blue = window.get_at((x, y)).b
-                red_1 = window.get_at((x + 1, y)).r
-                green_1 = window.get_at((x + 1, y)).g
-                blue_1 = window.get_at((x + 1, y)).b
-                likeness = distance(red, red_1, green, green_1, blue, blue_1)
-                pygame.draw.rect(window, (red_1, green_1, blue_1), (x + 1, y, likeness/3, 1), 1)#Uses the likeness value to determine the length of the rectangle
+        horizontal_lines()
                 
 # Press y for circles
-    b = (HEIGHT+4)/2
     if keys[pygame.K_y]:
-        for y in range(0,HEIGHT/2):
-            b -= 1
-            x = WIDTH/2
-            red = window.get_at((x, y)).r
-            green = window.get_at((x, y)).g
-            blue = window.get_at((x, y)).b
-            pygame.draw.circle(window,(red, green, blue),(WIDTH/2,HEIGHT/2),b ,1)
-    del px_array
+        circle()
+
+# Adds outline
+    if keys[pygame.K_u]:
+        outline()
 
 # Blits original picture
     if keys[pygame.K_SPACE]:
+        del px_array
         window.blit(picture_transformed, (0, 0))
     pygame.display.update()
